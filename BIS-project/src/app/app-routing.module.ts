bd9cf223ef862921ae9ecd5ecbe9cca1841addcd
Zamachi <32929482+Zamachi@users.noreply.github.com>
2021-04-29 14:24:14 +0200

    Setup ruta, Material-a, ikonica

--- BIS-project/src/app/app-routing.module.ts
+++ BIS-project/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//NOTE: import komponenti koje rutiramo
+
+const routes: Routes = [
+  //NOTE: sablon pisanja path: 'URL_putanja', component: ime_komponente
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
