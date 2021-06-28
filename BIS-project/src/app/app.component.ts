13683fdbc82944b9d58a5b70f7aaa69882aa8fe5
Iggyz0 <82712969+Iggyz0@users.noreply.github.com>
2021-06-28 02:48:06 +0200

    welcome page updated
    - added bg image
    - transparent toolbar
    - one model loading on the welcome page

--- BIS-project/src/app/app.component.ts
+++ BIS-project/src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  isLoggedIn$: any = false;
+  isDarkTheme: boolean = false;
+
   title = 'BIS-project';
+
+  toggleTheme() {
+    this.isDarkTheme = !this.isDarkTheme;
+  }
 }
