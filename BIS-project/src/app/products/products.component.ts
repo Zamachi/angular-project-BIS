8b12cebd721d619e027bca58ab91d33d762197e0
Zamachi <32929482+Zamachi@users.noreply.github.com>
2021-06-30 19:24:40 +0200

    Kategorije odradjene

--- BIS-project/src/app/products/products.component.ts
+++ BIS-project/src/app/products/products.component.ts
@@ -1,5 +1,5 @@
 import { LabelType, Options } from '@angular-slider/ngx-slider';
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { ProductModel } from '../models/productModel';
 import { ProductService } from '../services/product.service';
 
@@ -8,18 +8,14 @@ import { ProductService } from '../services/product.service';
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent implements OnInit, AfterViewInit {
 
   constructor(private productService: ProductService) { }
 
   catValue: any;
   subCatValue: any;
 
-  cat = new Map([
-    [ "key1", ["value1","value2"] ],
-    [ "key2", ["value3","value4"] ],
-    [ "key3", ["value5","value6"] ]
-  ]);
+  cat = new Map();
 
   minValuePrice = 1;
   maxValuePrice = 10000;
@@ -56,6 +52,31 @@ export class ProductsComponent implements OnInit {
 
   ngOnInit(): void {
     this.findAll();
+
+  }
+
+  ngAfterViewInit(): void {
+    this.initializeCategories();
+  }
+
+  initializeCategories(){
+    this.cat.clear();
+
+    let unique = [...new Set(this.data.map(item => item.category))];
+    let result = this.data.map(
+    item => ({ [item.category]:item.subCategory }) );
+
+    unique.forEach(
+      uniqueCat => {
+          let niz = [];
+          niz = result
+              .filter( testItem => Object.keys(testItem)[0] == uniqueCat )
+              .map( item => Object.values(item)[0] );
+          this.cat.set(uniqueCat, niz);
+       }
+  );
+
+
   }
 
   private findAll() {
@@ -63,12 +84,12 @@ export class ProductsComponent implements OnInit {
       if (result != null) {
         this.data = result;
       }
-    }).add(() => console.log(this.data));
+    }).add(() =>  {} );
   }
 
   singleProductDetails(id: string) {
     console.log(id);
-    
+
   }
 
   search(search: string) {
